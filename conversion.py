b45cbad7b401fabbe3b9f551b7d705b8f0536ea7
brycepanza <bep236@nau.edu>
2026-02-05 15:28:33 -0700

    Finished characteristic() function

--- conversion.py
+++ conversion.py
@@ -1,5 +1,15 @@
 def main():
-    print(characteristic("23.01"))
+    print(characteristic("231.01"))
+    print(characteristic(".9999"))
+    print(characteristic("hello world"))
+    print(characteristic("-10000.0"))
+    print(characteristic("1022a.50"))
+
+    print("Mantissa")
+    print(mantissa("233"))
+    print(mantissa("23.233434")
+    print(mantissa("12.1a24"))
+    print(mantissa("00a.122"))
 
 def characteristic(num_string):
     """
@@ -8,6 +18,18 @@ def characteristic(num_string):
     Returns:
         tuple: (bool, int) - (True, characteristic) if valid, (False, 0) if invalid
     """
+
+    sign = 1
+    # check for sign of value
+    if num_string[0] == '-':
+        sign = -1
+        num_string = num_string[1:]
+    # sign = -1 if num_string[0] == '-' else 1
+
+    # state variable for valid status of input string
+    is_valid = True
+    # variable for characteristic number
+    num = 0
     
     # attempt to convert character to number
     def atoi(char):
@@ -18,10 +40,27 @@ def characteristic(num_string):
         # send number as integer
         return unicode - ord('0')
 
-    print(atoi('1'))
-    print(atoi('0'))
-    print(atoi('9'))
-    print(atoi('{'))
+    # iterate for characters in string
+    for char in num_string:
+        # check for reached decimal point
+        if char == '.': break
+
+        # get value from character
+        curr_num = atoi(char)
+
+        # check for not found
+        if curr_num == None:
+            # set proper state
+            is_valid = False
+            num = 0
+            # exit procedure
+            break
+        print(curr_num)
+        num = 10 * num + curr_num
+
+    return (is_valid, sign * num)
+
+    
 
 def mantissa(num_string):
     """
